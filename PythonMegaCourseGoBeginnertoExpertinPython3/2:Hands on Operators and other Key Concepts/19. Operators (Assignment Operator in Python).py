8e9f3610b7faea140ee54936448f3e299acd6c3f
Cleison França Amorim <>
2021-06-16 06:27:57 -0400

    study

---
+++ PythonMegaCourseGoBeginnertoExpertinPython3/2:Hands on Operators and other Key Concepts/19. Operators (Assignment Operator in Python).py
@@ -0,0 +1,10 @@
+a =10
+a=a+1
+print(a)
+a += 1
+print(a)
+
+b = 10
+c=0
+b=c
+print(b)
---
+++ PythonMegaCourseGoBeginnertoExpertinPython3/2:Hands on Operators and other Key Concepts/21. Using of Assignments operator and Shorthand.py
@@ -0,0 +1,9 @@
+var = 2
+var +=10
+print(var)
+var *=10
+print(var)
+var /=10
+print(var)
+var -=10
+print(var)
