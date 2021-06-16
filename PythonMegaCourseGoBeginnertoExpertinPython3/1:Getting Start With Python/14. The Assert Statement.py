ae5a9ada1f1cd04eb253a35a69bfe7349911e068
Cleison França Amorim <>
2021-06-15 23:21:12 -0400

    study

---
+++ PythonMegaCourseGoBeginnertoExpertinPython3/1:Getting Start With Python/14. The Assert Statement.py
@@ -0,0 +1,6 @@
+# assert 4+4==8
+# # assert add(1,3) ==3 error
+
+a = 10
+assert a == 10
+assert a == 20
---
+++ PythonMegaCourseGoBeginnertoExpertinPython3/1:Getting Start With Python/16. Data Types In Python Major Concept.py
@@ -0,0 +1,11 @@
+a = 10
+print(type(a))
+
+b = 20.22
+print(type(b))
+
+print(type(5+8j))
+print(type(True))
+
+g="abc"
+print(type(g))
